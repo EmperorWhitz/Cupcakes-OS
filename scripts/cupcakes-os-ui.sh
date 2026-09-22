54316d9dd7e2203791d673062d85281bcc4240dd
Ryder <131211569+EmperorWhitz@users.noreply.github.com>
2026-09-22 02:27:57 +0000

    Update cupcakes-os-ui.sh

--- scripts/cupcakes-os-ui.sh
+++ scripts/cupcakes-os-ui.sh
@@ -16,7 +16,7 @@ _cupcakes_os_ui_resolve_version() {
         tr -d '[:space:]' < /etc/cupcakes-os/VERSION
         return
     fi
-    printf '4.1'
+    printf '4.2'
 }
 
 CUPCAKES_OS_UI_VERSION="$(_cupcakes_os_ui_resolve_version)"
